30c2cae3300ab268ed975d28114e8eb4dcfaf82d
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-06-13 16:38:16 +0300

    Refactor tests

--- tests.py
+++ tests.py
@@ -6,16 +6,16 @@ class MovieOOPTest(unittest.TestCase):
 
     def test_comedy(self):
         self.comedy = Comedy("Fist Fight", 91)
-        self.assertTrue(isinstance(self.comedy.play(), "Start laughing already because" \
-         + "Fist Fight is not going to spare your ribs!\n Length: 91"))
+        self.assertEqual(self.comedy.play(), "Start laughing already because " \
+         + "Fist Fight is not going to spare your ribs!\n Length: 91")
 
     def test_horror(self):
         self.horror = Horror("Ouija: Origin of Evil", 99)
-        self.assertTrue(isinstance(self.horror.play(), "Don't dare watch Ouija: Origin of Evil alone HUMAN! " \
-        "\n Length: 99"))
+        self.assertEqual(self.horror.play(), "Don't dare watch Ouija: Origin of Evil alone HUMAN! " \
+        "\n Length: 99")
     def test_realityshow(self):
         self.realityshow = RealityShow("Keeping up with the  Kardassians", 600)
-        self.assertTrue(isinstance(self.realityshow.play(), "Keeping up with the  Kardassians is Playing. Length: 600"))
+        self.assertEqual(self.realityshow.play(), "Keeping up with the  Kardassians is Playing. Length: 600")
 
 
 
